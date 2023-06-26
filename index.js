c57f9b245d1f70b746e6819edccd61f12b344f2b
Anthony O <antman@ANTMANS-MacBook-Air.local>
2023-06-26 14:05:33 -0400

    im trying

--- index.js
+++ index.js
@@ -1,1 +1,50 @@
 console.log("Code your solution!")
+
+const title = document.querySelector("h1");
+
+const todoListDiv = document.querySelector("div")
+
+  
+
+const newItemForlist = document.createElement("p");
+  newItemForlist.innerText= "list"
+  console.log("new pTag:" , newItemForlist)
+
+
+newItemForlist.addEventListener("mouseover", (event)=> (
+    console.log(event.target.innerText)
+))
+
+  todoListDiv.append(newItemForlist)
+
+
+  
+  
+  // const newItemForlist2 = document.createElement("p")
+  // newItemForlist2.innerText= "2."
+  // console.log("new ptag" ,newItemForlist2)
+ 
+  // todoListDiv.append(newItemForlist2)
+
+  
+  
+  
+const todoFormForNewItem = document.querySelector("form")
+  todoFormForNewItem.addEventListener("submit", (event)=>{
+    // console.log("submitting form !: ", event)
+    console.log("event form !: ", event.target[0].value)
+   todoListDiv.append(event.target[0].value)
+
+    event.preventDefault()
+
+  })
+
+  const newItemForlist3 = document.createElement("p")
+  newItemForlist2.innerText = ""
+  // console.log("new pTag" , newItemForlist3)
+  
+
+  todoListDiv.append(newItemForlist3)
+
+  todoFormForNewItem.reset()
+
